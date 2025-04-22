74a256e750e45139ca619d04f059ba512d70a951
LuisCruzA <LuisCruzA>
2025-04-22 12:47:59 -0600

    Navigator client added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 
 import NavigationProfessional from './components/Professional/NavigationProfessional';
+import NavigationClient from './components/Cliente/NavigationClient';
 
 import './global.css';
 
 export default function App() {
+  
+  let cliente: boolean=true;
+
+  
+  
   return (
-   
-  <NavigationProfessional />
+  <>
+  
+    {cliente ? <NavigationClient/> : <NavigationProfessional/>}
+    
+  </> 
+
+  
   
   );
 }
---
+++ Screens/Cliente/Profesionales.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Profesionales() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-semibold text-green-600">
+        Pantalla de profesionales
+      </Text>
+    </View>
+  );
+}

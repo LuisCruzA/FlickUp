ee547c26a1cb87079bc8705fd60290c6749a0137
Omar <omarnataren@outlook.com>
2025-04-22 21:37:35 -0600

    app fixed

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
-import { Amplify } from 'aws-amplify';
-import awsconfig from './src/aws-exports';
 import NavigationClient from './components/Cliente/NavigationClient';
 import NavigationProfessional from './components/Professional/NavigationProfessional';
+import { useState } from 'react';
 import LoginScreen from 'Screens/LoginScreen';
 import './global.css';
-
-Amplify.configure(awsconfig);
 
 export default function App() {
   const [usuario, setUsuario] = useState<null | { rol: string }>(null);
 
   if (!usuario) return <LoginScreen onLogin={setUsuario} />;
+
   return usuario.rol === 'cliente' ? <NavigationClient /> : <NavigationProfessional />;
 }
